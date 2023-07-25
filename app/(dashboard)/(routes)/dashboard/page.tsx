0eea6913c3e6c7af7ab6c8fb324b44b774c42645
shen.qin <shenqin@marketnode.com>
2023-07-25 19:48:40 +0800

    feat: init and add sidebar

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      DashboardPage
+      {/* <UserButton afterSignOutUrl="/" /> */}
+    </div>
+  );
+}
